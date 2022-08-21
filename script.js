b51d399bd1b0748d8b9c09ff1367c6fd8c8634d7
tiago <thiagoangelos2020@gmail.com>
2022-08-21 20:05:20 -0300

    1° push

---
+++ script.js
@@ -0,0 +1,456 @@
+/*SCRIPT PURO PARA GERAR UMA FRASE ALEATÓRIA DO PENSADOR COM NUMERO GERADO ALEATORIAMENTE COM O 'Math.random' e Com
+A Estrutura Do Switch Escolha. ----> script Para Fazer Uma API JSON FUTURAMENTE Reaprovaitar Dados. */
+
+function gerar_frase(){
+	function gerarNumero(x, y){
+		return Math.floor(Math.random() * (y - x + 1)) + x
+
+	}
+
+	var num = (gerarNumero(1, 110));
+
+	switch(num){
+		case 1:
+			document.getElementById('frase_retornada').innerHTML = "Preocupe-se mais com a sua consciência do que com sua reputação. Porque sua consciência é o que você é, e a sua reputação é o que os outros pensam de você."
+		break;
+
+		case 2:
+			document.getElementById('frase_retornada').innerHTML = "Exige muito de ti e espera pouco dos outros. Assim, evitarás muitos aborrecimentos."
+		break;
+
+		case 3:
+			document.getElementById('frase_retornada').innerHTML = "Há um tempo em que é preciso abandonar as roupas usadas, que já tem a forma do nosso corpo, e esquecer os nossos caminhos, que nos levam sempre aos mesmos lugares. É o tempo da travessia: e, se não ousarmos fazê-la, teremos ficado, para sempre, à margem de nós mesmos."
+		break;
+
+		case 4:
+			document.getElementById('frase_retornada').innerHTML = "A saudade é o que faz as coisas pararem no tempo."
+		break;
+
+		case 5:
+			document.getElementById('frase_retornada').innerHTML = "Os homens de poucas palavras são os melhores."
+		break;
+
+		case 6:
+			document.getElementById('frase_retornada').innerHTML = "Há três coisas na vida que nunca voltam atrás: a flecha lançada, a palavra pronunciada e a oportunidade perdida."
+		break;
+
+		case 7:
+			document.getElementById('frase_retornada').innerHTML = "Cada qual sabe amar a seu modo; o modo, pouco importa; o essencial é que saiba amar."
+		break;
+
+		case 8:
+			document.getElementById('frase_retornada').innerHTML = "Tente mover o mundo – o primeiro passo será mover a si mesmo."
+		break;
+
+		case 9:
+			document.getElementById('frase_retornada').innerHTML = "Sábio é aquele que conhece os limites da própria ignorância."
+		break;
+
+		case 10:
+			document.getElementById('frase_retornada').innerHTML = "Esquecer é uma necessidade. A vida é uma lousa, em que o destino, para escrever um novo caso, precisa de apagar o caso escrito."
+		break;
+
+		case 11:
+			document.getElementById('frase_retornada').innerHTML = "Aja antes de falar e, portanto, fale de acordo com os seus atos."
+		break;
+
+		case 12:
+			document.getElementById('frase_retornada').innerHTML = "Sofremos muito com o pouco que nos falta e gozamos pouco o muito que temos."
+		break;
+
+		case 13:
+			document.getElementById('frase_retornada').innerHTML = "A vida é muito importante para ser levada a sério."
+		break;
+
+		case 14:
+			document.getElementById('frase_retornada').innerHTML = "Podemos facilmente perdoar uma criança que tem medo do escuro; a real tragédia da vida é quando os homens têm medo da luz.."
+		break;
+
+		case 15:
+			document.getElementById('frase_retornada').innerHTML = "Que ninguém se engane, só se consegue a simplicidade através de muito trabalho."
+		break;
+
+		case 16:
+			document.getElementById('frase_retornada').innerHTML = "Torna-te aquilo que és."
+		break;
+
+		case 17:
+			document.getElementById('frase_retornada').innerHTML = "Se queres prever o futuro, estuda o passado."
+		break;
+
+		case 18:
+			document.getElementById('frase_retornada').innerHTML = "A arte de viver é simplesmente a arte de conviver... simplesmente, disse eu? Mas como é difícil!"
+		break;
+
+		case 19:
+			document.getElementById('frase_retornada').innerHTML = "Só se vê bem com o coração, o essencial é invisível aos olhos."
+		break;
+
+		case 20:
+			document.getElementById('frase_retornada').innerHTML = "O amor calcula as horas por meses, e os dias por anos; e cada pequena ausência é uma eternidade."
+		break;
+
+		case 21:
+			document.getElementById('frase_retornada').innerHTML = "A vida vai ficando cada vez mais dura perto do topo."
+		break;
+
+		case 22:
+			document.getElementById('frase_retornada').innerHTML = "Somos feitos de carne, mas temos de viver como se fôssemos de ferro."
+		break;
+
+		case 23:
+			document.getElementById('frase_retornada').innerHTML = "É mais fácil lidar com uma má consciência do que com uma má reputação."
+		break;
+
+		case 24:
+			document.getElementById('frase_retornada').innerHTML = "Tudo é precioso para aquele que foi, por muito tempo, privado de tudo.Friedrich Nietzsche3.2 mil compartilhamentos"
+		break;
+
+		case 25:
+			document.getElementById('frase_retornada').innerHTML = "Ser grande é abraçar uma grande causa."
+		break;
+
+		case 26:
+			document.getElementById('frase_retornada').innerHTML = "Somente quando encontramos o amor, é que descobrimos o que nos faltava na vida."
+		break;
+
+		case 27:
+			document.getElementById('frase_retornada').innerHTML = "É preciso ter o caos dentro de si para gerar uma estrela dançante."
+		break;
+
+		case 28:
+			document.getElementById('frase_retornada').innerHTML = "Na vingança e no amor a mulher é mais bárbara do que o homem."
+		break;
+
+		case 29:
+			document.getElementById('frase_retornada').innerHTML = "Com organização e tempo, acha-se o segredo de fazer tudo e bem feito."
+		break;
+
+		case 30:
+			document.getElementById('frase_retornada').innerHTML = "O maior erro que você pode cometer é o de ficar o tempo todo com medo de cometer algum."
+		break;
+
+		case 31:
+			document.getElementById('frase_retornada').innerHTML = "A vida só pode ser compreendida olhando-se para trás, mas só pode ser vivida olhando-se para a frente."
+		break;
+
+		case 32:
+			document.getElementById('frase_retornada').innerHTML = "A gratidão é o único tesouro dos humildes."
+		break;
+
+		case 33:
+			document.getElementById('frase_retornada').innerHTML = "Quem quiser vencer na vida deve fazer como os seus sábios: mesmo com a alma partida, ter um sorriso nos lábios."
+		break;
+
+		case 34:
+			document.getElementById('frase_retornada').innerHTML = "Apressa-te a viver bem e pensa que cada dia é, por si só, uma vida."
+		break;
+
+		case 35:
+			document.getElementById('frase_retornada').innerHTML = "Jamais se desespere em meio as sombrias aflições de sua vida, pois das nuvens mais negras cai água límpida e fecunda."
+		break;
+
+		case 36:
+			document.getElementById('frase_retornada').innerHTML = "Sem a música a vida seria um erro."
+		break;
+
+		case 37:
+			document.getElementById('frase_retornada').innerHTML = "É muito melhor viver sem felicidade do que sem amor."
+		break;
+
+		case 38:
+			document.getElementById('frase_retornada').innerHTML = "O descontentamento é o primeiro passo na evolução de um homem ou de uma nação."
+		break;
+
+		case 39:
+			document.getElementById('frase_retornada').innerHTML = "A alma é essa coisa que nos pergunta se a alma existe."
+		break;
+
+		case 40:
+			document.getElementById('frase_retornada').innerHTML = "Se fiz alguma coisa boa em toda a minha vida, dela me arrependo do fundo do coração."
+		break;
+		
+		case 41:
+			document.getElementById('frase_retornada').innerHTML = "Olho por olho, e o mundo acabará cego."
+		break;
+
+		case 42:
+			document.getElementById('frase_retornada').innerHTML = "E um dia os homens descobrirão que esses discos voadores estavam apenas estudando a vida dos insetos..."
+		break;
+
+		case 43:
+			document.getElementById('frase_retornada').innerHTML = "Bendito quem inventou o belo truque do calendário, pois o bom da segunda-feira, do dia 1.º do mês e de cada ano novo é que nos dão a impressão de que a vida não continua, mas apenas recomeça..."
+		break;
+
+		case 44:
+			document.getElementById('frase_retornada').innerHTML = "Um homem que está livre da religião tem uma oportunidade melhor de viver uma vida mais normal e completa."
+		break;
+		
+		case 45:
+			document.getElementById('frase_retornada').innerHTML = "Aprendi através da experiência amarga a suprema lição: controlar minha ira e torná-la como o calor que é convertido em energia. Nossa ira controlada pode ser convertida numa força capaz de mover o mundo."
+		break;
+
+		case 46:
+			document.getElementById('frase_retornada').innerHTML = "Sonhar é acordar-se para dentro."
+		break;
+
+		case 47:
+			document.getElementById('frase_retornada').innerHTML = "Meu conselho é que se case. Se você arrumar uma boa esposa, será feliz; se arrumar uma esposa ruim, se tornará um filósofo."
+		break;
+
+		case 48:
+			document.getElementById('frase_retornada').innerHTML = "Na plenitude da felicidade, cada dia é uma vida inteira."
+		break;
+
+		case 49:
+			document.getElementById('frase_retornada').innerHTML = "Um sonho sonhado sozinho é um sonho. Um sonho sonhado junto é realidade."
+		break;
+		
+		case 50:
+			document.getElementById('frase_retornada').innerHTML = "Ser grande significa ser incompreendido."
+		break;
+
+		case 51:
+			document.getElementById('frase_retornada').innerHTML = "Viver é desenhar sem borracha."
+		break;
+
+		case 52:
+			document.getElementById('frase_retornada').innerHTML = "O saber a gente aprende com os mestres e os livros. A sabedoria se aprende é com a vida e com os humildes."
+		break;
+
+		case 53:
+			document.getElementById('frase_retornada').innerHTML = "Quanto menos inteligente um homem é, menos misteriosa lhe parece a existência."
+		break;
+
+		case 54:
+			document.getElementById('frase_retornada').innerHTML = "O homem de palavra fácil e personalidade agradável raras vezes é homem de bem."
+		break;
+
+		case 55:
+			document.getElementById('frase_retornada').innerHTML = "Cada dia a natureza produz o suficiente para nossa carência. Se cada um tomasse o que lhe fosse necessário, não havia pobreza no mundo e ninguém morreria de fome."
+		break;
+
+		case 56:
+			document.getElementById('frase_retornada').innerHTML = "Fortes razões, fazem fortes ações."
+		break;
+
+		case 57:
+			document.getElementById('frase_retornada').innerHTML = "Nas grandes batalhas da vida, o primeiro passo para a vitória é o desejo de vencer."
+		break;
+
+		case 58:
+			document.getElementById('frase_retornada').innerHTML = "Para os crentes, Deus está no princípio das coisas. Para os cientistas, no final de toda reflexão."
+		break;
+
+		case 59:
+			document.getElementById('frase_retornada').innerHTML = "A experiência é o nome que damos aos nossos erros."
+		break;
+
+		case 60:
+			document.getElementById('frase_retornada').innerHTML = "Se o dinheiro for a sua esperança de independência, você jamais a terá. A única segurança verdadeira consiste numa reserva de sabedoria, de experiência e de competência."
+		break;
+
+		case 61:
+			document.getElementById('frase_retornada').innerHTML = "Se vives de acordo com as leis da natureza, nunca serás pobre; se vives de acordo com as opiniões alheias, nunca serás rico."
+		break;
+
+		case 62:
+			document.getElementById('frase_retornada').innerHTML = "O número dos que nos invejam confirma as nossas capacidades."
+		break;
+
+		case 63:
+			document.getElementById('frase_retornada').innerHTML = "Na vida todos temos um segredo inconfessável, um arrependimento irreversível, um sonho inalcançável e um amor inesquecível."
+		break;
+
+		case 64:
+			document.getElementById('frase_retornada').innerHTML = "A vida é uma simples sombra que passa (...); é uma história contada por um idiota, cheia de ruído e de furor e que nada significa."
+		break;
+
+		case 65:
+			document.getElementById('frase_retornada').innerHTML = "A cultura é o melhor conforto para a velhice."
+		break;
+
+		case 66:
+			document.getElementById('frase_retornada').innerHTML = "As dificuldades são o aço estrutural que entra na construção do caráter."
+		break;
+
+		case 67:
+			document.getElementById('frase_retornada').innerHTML = "É sobretudo na solidão que se sente a vantagem de viver com alguém que saiba pensar."
+		break;
+
+		case 68:
+			document.getElementById('frase_retornada').innerHTML = "Para a maioria, quão pequena é a porção de prazer que basta para fazer a vida agradável!"
+		break;
+
+		case 69:
+			document.getElementById('frase_retornada').innerHTML = "Seja como os pássaros que, ao pousarem um instante sobre ramos muito leves, sentem-nos ceder, mas cantam! Eles sabem que possuem asas."
+		break;
+
+		case 70:
+			document.getElementById('frase_retornada').innerHTML = "O pensamento é escravo da vida, e a vida é o bobo do tempo."
+		break;
+
+		case 71:
+			document.getElementById('frase_retornada').innerHTML = "Viver sem filosofar é o que se chama ter os olhos fechados sem nunca os haver tentado abrir."
+		break;
+
+		case 72:
+			document.getElementById('frase_retornada').innerHTML = "As três coisas mais difíceis do mundo são: guardar um segredo, perdoar uma ofensa e aproveitar o tempo."
+		break;
+
+		case 73:
+			document.getElementById('frase_retornada').innerHTML = "A amizade perfeita apenas pode existir entre os bons."
+		break;
+
+		case 74:
+			document.getElementById('frase_retornada').innerHTML = "O amor deveria perdoar todos os pecados, menos um pecado contra o amor. O amor verdadeiro deveria ter perdão para todas as vidas, menos para as vidas sem amor."
+		break;
+
+		case 75:
+			document.getElementById('frase_retornada').innerHTML = "Um amor bem verdadeiro, uma vida bem íntima com uma mulher, a quem se queira como amante, que se estime como irmã, que se venere com mãe, que se proteja como filha, é evidentemente o destino mais natural ao homem, o complemento da sua missão na terra."
+		break;
+
+		case 76:
+			document.getElementById('frase_retornada').innerHTML = "A maior lição da vida é a de que, às vezes, até os tolos têm razão."
+		break;
+
+		case 77:
+			document.getElementById('frase_retornada').innerHTML = "Quando, na vida, uma porta se fecha para nós, há sempre outra que nos abre. Em geral, porém, olhamos com tanto pesar e ressentimento para a porta fechada, que não nos apercebemos da outra que se abriu."
+		break;
+
+		case 78:
+			document.getElementById('frase_retornada').innerHTML = "Um único minuto de reconciliação vale mais do que toda uma vida de amizade."
+		break;
+
+		case 79:
+			document.getElementById('frase_retornada').innerHTML = "O saber se aprende com os mestres. A sabedoria, só com o corriqueiro da vida."
+		break;
+
+		case 80:
+			document.getElementById('frase_retornada').innerHTML = "O mestre disse: Quem se modera, raramente se perde."
+		break;
+
+		case 81:
+			document.getElementById('frase_retornada').innerHTML = "Guarda teu amigo sob a chave de tua própria vida."
+		break;
+
+		case 82:
+			document.getElementById('frase_retornada').innerHTML = "Na vida do homem, o amor é uma parte, na da mulher, é toda a vida."
+		break;
+
+		case 83:
+			document.getElementById('frase_retornada').innerHTML = "A suprema felicidade da vida é a convicção de ser amado por aquilo que você é, ou melhor, apesar daquilo que você é."
+		break;
+
+		case 84:
+			document.getElementById('frase_retornada').innerHTML = "A vida é uma pedra de amolar: desgasta-nos ou afia-nos, conforme o metal de que somos feitos."
+		break;
+
+		case 85:
+			document.getElementById('frase_retornada').innerHTML = "O Estado deve fazer o que é útil. O indivíduo deve fazer o que é belo"
+		break;
+
+		case 86:
+			document.getElementById('frase_retornada').innerHTML = "A amizade é como os títulos honoríficos: quanto mais velha, mais preciosa."
+		break;
+
+		case 87:
+			document.getElementById('frase_retornada').innerHTML = "A vida é como um sonho; é o acordar que nos mata."
+		break;
+
+		case 88:
+			document.getElementById('frase_retornada').innerHTML = "Certas coisas só são amargas se a gente as engole."
+		break;
+
+		case 89:
+			document.getElementById('frase_retornada').innerHTML = "O diabo desta vida é que entre cem caminhos temos que escolher apenas um, e viver com a nostalgia dos outros noventa e nove."
+		break;
+
+		case 90:
+			document.getElementById('frase_retornada').innerHTML = "Os homens dizem que a vida é curta, e eu vejo que eles se esforçam para a tornar assim."
+		break;
+
+		case 91:
+			document.getElementById('frase_retornada').innerHTML = "Somos o que fazemos, mas somos, principalmente, o que fazemos para mudar o que somos."
+		break;
+
+		case 92:
+			document.getElementById('frase_retornada').innerHTML = "A mudança é a lei da vida. E aqueles que apenas olham para o passado ou para o presente irão com certeza perder o futuro."
+		break;
+
+		case 93:
+			document.getElementById('frase_retornada').innerHTML = "Fazer aniversário é olhar para trás com gratidão e para a frente com fé!"
+		break;
+
+		case 94:
+			document.getElementById('frase_retornada').innerHTML = "Alguns homens vêem as coisas como são, e dizem 'Por quê?' Eu sonho com as coisas que nunca foram e digo 'Por que não?'"
+		break;
+
+		case 95:
+			document.getElementById('frase_retornada').innerHTML = "Os homens semeiam na terra o que colherão na vida espiritual: os frutos da sua coragem ou da sua fraqueza."
+		break;
+
+		case 96:
+			document.getElementById('frase_retornada').innerHTML = "Minha vida é uma colcha de retalhos. Todos da mesma cor."
+		break;
+
+		case 97:
+			document.getElementById('frase_retornada').innerHTML = "A verdadeira amizade é como a saúde: o seu valor só é reconhecido quando a perdemos."
+		break;
+
+		case 98:
+			document.getElementById('frase_retornada').innerHTML = "O mais feliz dos felizes é aquele que faz os outros felizes."
+		break;
+
+		case 99:
+			document.getElementById('frase_retornada').innerHTML = "A vida é um campo de urtigas onde a única rosa é o amor."
+		break;
+
+		case 100:
+			document.getElementById('frase_retornada').innerHTML = "Devemos aprender durante toda a vida, sem imaginar que a sabedoria vem com a velhice."
+		break;
+
+		case 101:
+			document.getElementById('frase_retornada').innerHTML = "Muita gente estraga a vida com um doentio e exagerado altruísmo."
+		break;
+
+		case 102:
+			document.getElementById('frase_retornada').innerHTML = "Posso resumir em três palavras o que aprendi sobre a vida: a vida continua."
+		break;
+
+		case 103:
+			document.getElementById('frase_retornada').innerHTML = "A vida real do ser humano consiste em ser feliz, principalmente por estar sempre na esperança de sê-lo muito em breve."
+		break;
+
+		case 104:
+			document.getElementById('frase_retornada').innerHTML = "A nossa vida é aquilo que os nossos pensamentos fizerem dela."
+		break;
+
+		case 105:
+			document.getElementById('frase_retornada').innerHTML = "Quanto mais amor temos, tanto mais fácil fazemos a nossa passagem pelo mundo."
+		break;
+
+		case 106:
+			document.getElementById('frase_retornada').innerHTML = "A vida sem uma meta é completamente vazia."
+		break;
+
+		case 107:
+			document.getElementById('frase_retornada').innerHTML = "Existem dois objetivos na vida: o primeiro, o de obter o que desejamos; o segundo, o de desfrutá-lo. Apenas os homens mais sábios realizam o segundo."
+		break;
+
+		case 108:
+			document.getElementById('frase_retornada').innerHTML = "É melhor que fale por nós a nossa vida, que as nossas palavras."
+		break;
+
+		case 109:
+			document.getElementById('frase_retornada').innerHTML = "Quando somos bons para os outros, somos ainda melhores para nós."
+		break;
+
+		case 110:
+			document.getElementById('frase_retornada').innerHTML = "A vida se contrai e se expande proporcionalmente à coragem do indivíduo."
+		break;
+
+	}
+
+}
+
